d30dc4f037bb04d7159c56573132d0f5a283446b
Tanmayi <45629438+Tanmayi03@users.noreply.github.com>
2020-06-10 20:39:52 +0530

    Tried to remove right padding of navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./style.css";
 function NavBar() {
   return (
-    <div className="about-container">
+    <div style={{ paddingRight: '0px !important'}}className="about-container">
       <span className="about-text">About</span>
       <span className="about-text">Projects</span>
       <span className="about-text">Team</span>
