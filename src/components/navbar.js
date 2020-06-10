4d497fbddf30be3aa6e502f1fd47ff769fd3e1e4
Tanmayi <45629438+Tanmayi03@users.noreply.github.com>
2020-06-10 21:53:36 +0530

    Used External styling

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./style.css";
 function NavBar() {
   return (
-    <div style={{ paddingRight: '0px !important'}}className="about-container">
+    <div className="about-container">
       <span className="about-text">About</span>
       <span className="about-text">Projects</span>
       <span className="about-text">Team</span>
