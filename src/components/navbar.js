ae136bca23e30f97039c10908dd51fe9f68940df
riyasavant <rieee2110@gmail.com>
2020-08-08 12:56:28 +0530

    Reconstructed NavBar and Dark-Theme Button

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,12 +1,23 @@
 import React from "react";
 import "./style.css";
-function NavBar() {
+import 'bootstrap/dist/css/bootstrap.css';
+
+function NavBar({onClick, darkTheme}) {
   return (
-    <div className="about-container">
-      <span><a href="#about" className="about-text">About</a></span>
-      <span><a href="#projects" className="about-text">Projects</a></span>
-      <span><a href="#team" className="about-text">Team</a></span>
-    </div>
+    <nav class="navbar navbar-expand-lg navbar-dark custom-color">
+      <a class="navbar-brand custom-link" href="#">Codesis</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav ml-auto">
+          <a class="nav-link active custom-link" href="#">About <span class="sr-only">(current)</span></a>
+          <a class="nav-link custom-link" href="#">Projects</a>
+          <a class="nav-link custom-link" href="#">Team</a>
+          <button className="btn btn-outline-warning" onClick={onClick}>{darkTheme? 'Light':'Dark'}</button>
+        </div>
+      </div>
+    </nav>
   );
 }
 export default NavBar;
